8b017b8b98df6b9310e05e3df4db109b6d363663
Aditya Gour <aditya9051kumar@gmail.com>
2024-09-28 21:21:50 +0530

    adding the project of view binding

---
+++ app/src/main/java/com/example/viewbinding/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.viewbinding
+
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.viewbinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.button.setOnClickListener {
+            if (binding.checkBox.isChecked) { finish()
+            } else {
+                binding.checkBox.buttonTintList = ColorStateList.valueOf(Color.RED)
+                Toast.makeText(this, "Please accept the conditions", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+}
